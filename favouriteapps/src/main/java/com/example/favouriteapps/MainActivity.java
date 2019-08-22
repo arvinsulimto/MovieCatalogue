25b3e69114db562e104b531edff6e6690295a386
Arvin Sulimto <arvinsulimto@gmail.com>
2019-08-22 22:43:20 +0700

    Initial Commit v 1.5

--- favouriteapps/src/main/java/com/example/favouriteapps/MainActivity.java
+++ favouriteapps/src/main/java/com/example/favouriteapps/MainActivity.java
@@ -28,10 +28,15 @@ public class MainActivity extends AppCompatActivity {
         String sortOrder = null;
 
         Cursor cursor = resolver.query(uri, projection, selection, selectionArgs, sortOrder);
-
+        int data = 0;
         while (cursor.moveToNext()) {
             String name = cursor.getString(cursor.getColumnIndex("Name"));
+            data++;
             dataSet.add(name);
+        }
+
+        if(data ==0){
+            dataSet.add(getResources().getString(R.string.noData));
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataSet);
